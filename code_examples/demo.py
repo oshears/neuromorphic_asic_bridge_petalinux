0a7d402138744a3ca033bf59d04ef929cba6002b
Osaze Shears <oshears@vt.edu>
2021-01-07 11:14:33 -0500

    updated demo code

--- code_examples/demo.py
+++ code_examples/demo.py
@@ -1,5 +1,6 @@
 import os
 import mmap
+import time
 
 
 mem_file = os.open("/dev/mem", os.O_SYNC | os.O_RDWR)
@@ -13,24 +14,33 @@ DIRECT_CTRL_REG = 8
 DBG_REG = 12
 
 DBG_REG_1HZ = 0x8
-DBG_REG_DIRECT_CTRL_OUT = 0x4
+DBG_REG_DIRECT_CTRL_DIGIT_OUT = 0x4
 DBG_REG_DIRECT_CTRL_LED_OUT = 0x2
 DBG_REG_CHAR_SELECT_LED_OUT = 0x1
 
 # Change Frequency to 1HZ
-neuromorphic_bridge_registers[DBG_REG] = 0x8
+# Display Char Select Reg Outputs on LEDS
+neuromorphic_bridge_registers[DBG_REG] = DBG_REG_1HZ | DBG_REG_CHAR_SELECT_LED_OUT
+print(f"Debug Register: { neuromorphic_bridge_registers[DBG_REG] }")
 
-# Char Select Register
-for i in range(16):
+# Test Char Select Register
+for i in range(4):
     neuromorphic_bridge_registers[CHAR_SEL_REG] = i
-    print(neuromorphic_bridge_registers[CHAR_SEL_REG])
-    print(f"Character Select Register: {neuromorphic_bridge_registers[CHAR_SEL_REG]}")
+    print(f"Character Select Register: { neuromorphic_bridge_registers[CHAR_SEL_REG] }")
+    time.sleep(5)
 
 # Network Output Register
-print(f"Network Output Register: {neuromorphic_bridge_registers[NET_OUT_REG]}")
+print(f"Network Output Register: { neuromorphic_bridge_registers[NET_OUT_REG] }")
 
-# Debug Register
-# for i in range(16):
-#     neuromorphic_bridge_registers[DBG_REG] = i
-#     print(neuromorphic_bridge_registers[DBG_REG])
-#     print(f"Debug Register: {neuromorphic_bridge_registers[DBG_REG]}")
+# Change Frequency to 1HZ
+# Display Direct Control Reg Outputs on LEDS
+# Place Direct Control Outputs on Digit Outputs
+neuromorphic_bridge_registers[DBG_REG] = DBG_REG_1HZ | DBG_REG_DIRECT_CTRL_LED_OUT | DBG_REG_DIRECT_CTRL_DIGIT_OUT
+print(f"Debug Register: { neuromorphic_bridge_registers[DBG_REG] }")
+
+
+# Test Direct Control Register
+for i in range(2**16):
+    neuromorphic_bridge_registers[DIRECT_CTRL_REG] = i
+    print(f"Direct Control Register: { neuromorphic_bridge_registers[DIRECT_CTRL_REG] }")
+    time.sleep(0.1)
